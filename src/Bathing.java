5ce181fae7cedcb90253da9f61463973894e6e1a
haeinju <94752412+haeinju@users.noreply.github.com>
2021-11-23 17:46:47 +0000

    Add files via upload

---
+++ src/Bathing.java
@@ -0,0 +1,47 @@
+
+import java.sql.*;
+
+public class Bathing {
+	
+	public void putBathingInfo(Connection conn, BathingInfo info) {
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement("INSERT INTO Bathing VALUES (?,?,?,?,?,?)");
+			pstmt.setByte(0, info.hasHairDryer);
+			pstmt.setByte(1, info.hasShampoo);
+			pstmt.setByte(2, info.hasToiletpaper);
+			pstmt.setInt(3, info.bathroomNum);
+			pstmt.setInt(4, info.facilityNo);
+			pstmt.setInt(5, info.bathroomNo);
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public BathingInfo getBathingInfo(Connection conn, int facilityNo) {
+		BathingInfo info = new BathingInfo();
+		info.facilityNo = facilityNo;
+		
+		try {
+			String sql = "SELECT * FROM Bathing WHERE facilityNo =" + info.facilityNo;
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				info.hasHairDryer = rs.getByte("hasHairDryer");
+				info.hasShampoo = rs.getByte("hasShampoo");
+				info.hasToiletpaper = rs.getByte("hasToiletpaper");
+				info.bathroomNum = rs.getInt("bathroomNum");
+				info.facilityNo = rs.getInt("facilityNo");
+				info.bathroomNo = rs.getInt("bathroomNo");
+			}
+			
+			rs.close();
+			stmt.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return info;
+	}
+}
---
+++ src/BathingInfo.java
@@ -0,0 +1,9 @@
+
+public class BathingInfo {
+	public byte hasHairDryer;
+	public byte hasShampoo;
+	public byte hasToiletpaper;
+	public int bathroomNum;
+	public int facilityNo;
+	public int bathroomNo;
+}
