75b7af5970cebbce3c4ef74b28045d950784fb53
haeinju <94752412+haeinju@users.noreply.github.com>
2021-11-23 17:34:06 +0000

    Add files via upload

---
+++ src/Facility.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Facility {
+	public void insertFacilityNo(Connection conn) {
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement("INSERT INTO Facility Values default")
+			//auto-increment
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ src/Sleeping.java
@@ -0,0 +1,44 @@
+
+import java.sql.*;
+
+public class Sleeping {
+	
+	public void putSleepingInfo(Connection conn, int facilityNo, SleepingInfo info) {
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement("INSERT INTO Sleeping VALUES (?,?,?,?,?)");
+			pstmt.setByte(0, info.hasBedLinen);
+			pstmt.setByte(1, info.hasTowel);
+			pstmt.setInt(2, info.bedroomsNum);
+			pstmt.setInt(3, info.facilityNo);
+			pstmt.setInt(4, info.bedroomNo);
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public SleepingInfo getSleepingInfo(Connection conn, int facilityNo) {
+		SleepingInfo info = new SleepingInfo();
+		info.facilityNo = facilityNo;
+		
+		try {
+			String sql = "SELECT * FROM Sleeping WHERE facilityNo =" + info.facilityNo;
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				info.hasBedLinen = rs.getByte("hasBedLinen");
+				info.hasTowel = rs.getByte("hasTowel");
+				info.bedroomsNum = rs.getInt("bedroomsNum");
+				info.bedroomNo = rs.getInt("bedroomNo");
+			}
+			
+			rs.close();
+			stmt.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return info;
+	}
+}
