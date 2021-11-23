94e40a6ed6f988b8b12c589846f9b2e030f5c35c
haeinju <94752412+haeinju@users.noreply.github.com>
2021-11-23 18:19:48 +0000

    Add files via upload

---
+++ src/KitchenInfo.java
@@ -0,0 +1,12 @@
+
+public class KitchenInfo {
+	public byte hasRefrigerator;
+	public byte hasMicrowave;
+	public byte hasOven;
+	public byte hasStove;
+	public byte hasDishwasher;
+	public byte hasTableware;
+	public byte hasCookware;
+	public byte hasBasicProvisions;
+	public int facilityNo;
+}
---
+++ src/LivingInfo.java
@@ -0,0 +1,10 @@
+
+public class LivingInfo {
+	public byte hasWifi;
+	public byte hasTV;
+	public byte hasSatellite;
+	public byte hasStreaming;
+	public byte hasDvdPlayer;
+	public byte hasBoardGames;
+	public int facilityNo;
+}
