ba1dc467334a84d4e434a6a98869b2d2365a72b1
Yoorim <ycho12@sheffield.ac.uk>
2021-11-26 16:35:34 +0900

    add screen

--- src/sql.java
+++ src/sql.java
@@ -1,16 +1,17 @@
 import java.sql.*;
 
+
 public class sql {
     static String URL = "jdbc:mysql://stusql.dcs.shef.ac.uk/";
     static String DBNAME = "team062";
     static String USER = "team062";
     static String PASSWORD = "10293718";
-
-    public static void main(String[] args) {
-        DBAccess.connect();
-        System.out.println("Connected");
-        DBAccess.disconnect();
-        System.out.println("Disconnected");
-    }
+    
+//    public static void main(String[] args) {
+//        DBAccess.connect();
+//        System.out.println("Connected");
+//        DBAccess.disconnect();
+//        System.out.println("Disconnected");
+//    }
 
 }
