f81686a7dd38cf72ee01d6187acb9ca5d0160dc3
haeinju <94752412+haeinju@users.noreply.github.com>
2021-11-28 15:21:20 +0000

    Update Facility.java

--- src/Facility.java
+++ src/Facility.java
@@ -1,7 +1,16 @@
+import Sleeping;
+import Bedroom;
+import Bathing;
+import Bathroom;
+import Kitchen;
+import Living;
+import Utility;
+import Outdoor;
+
 import java.sql.*;
 
 public class Facility {
-	public void insertFacilityNo(Connection conn) {
+	public static void insertFacilityNo(Connection conn) {
 		PreparedStatement pstmt = null;
 		try {
 			pstmt = conn.prepareStatement("INSERT INTO Facility Values default");
@@ -10,5 +19,27 @@ public class Facility {
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	public static void registerFacilityInfo(
+			Connection conn, 
+			SleepingInfo sleepingInfo,
+			BedroomInfo bedroomInfo,
+			BathingInfo bathingInfo,
+			BathroomInfo bathroomInfo,
+			KitchenInfo kitchenInfo,
+			LivingInfo livingInfo,
+			UtilityInfo utilityInfo,
+			OutdoorInfo outdoorInfo) {
+		
+		Sleeping.putSleepingInfo(conn, sleepingInfo);
+		Bedroom.putBedroomInfo(conn, bedroomInfo);
+		Bathing.putBathingInfo(conn, bathingInfo);
+		Bathroom.putBathroomInfo(conn, bathroomInfo);
+		Kitchen.putKitchenInfo(conn, kitchenInfo);
+		Living.putLivingInfo(conn, livingInfo);
+		Utility.putUtilityInfo(conn, utilityInfo);
+		Outdoor.putOutdoorInfo(conn, outdoorInfo);
+	    
 	}
 }
