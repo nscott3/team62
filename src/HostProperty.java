b3f78e9d8803354c3c03adf436d98bc497849208
edragoti01 <edragoti1@sheffield.ac.uk>
2021-11-28 18:55:05 +0000

    Made classes for the location and sizes and started out the facilities, haven't done any action listeners yet. Will do when finished with the checkboxes.

--- src/HostProperty.java
+++ src/HostProperty.java
@@ -5,8 +5,8 @@ import static sun.plugin.javascript.navig.JSType.Location;
 
 public class HostProperty extends JFrame {
     String Answers[]={"Yes","No"};
-    private JPanel HostPropertyPanel = new JPanel();
-
+    String bathing[]={"Towel", "Linen"};
+    private JPanel contentPane;
     private JLabel propertyNameLabel=new JLabel("Enter the properties name:");
     private JTextField propertyName = new JTextField();
     private JLabel propertyDescLabel=new JLabel("Enter the properties description:");
@@ -15,19 +15,13 @@ public class HostProperty extends JFrame {
     private JTextField propertyLocation = new JTextField();
     private JLabel hasBreakfastLabel=new JLabel("Does it have breakfast?");
     private JComboBox hasBreakfast= new JComboBox(Answers);
+    private JLabel sleepingFacilityLabel= new JLabel("Tick the which sleeping facilities are available");
+    private JCheckBox hasLinen = new JCheckBox();
+    private JCheckBox hasTowel = new JCheckBox();
 
 
 
-
-
-    public  HostProperty(){
-
-        super("Host");
-        Container contentPane=getContentPane();
-        contentPane.setLayout(null);
-
-
-
+    public void setLocationAndSize(){
         propertyNameLabel.setBounds(50,50,200,25);
         propertyName.setBounds(250,50,200,25);
         propertyDescLabel.setBounds(50,50,250,125);
@@ -36,23 +30,45 @@ public class HostProperty extends JFrame {
         propertyLocation.setBounds(250,225,250,25);
         hasBreakfastLabel.setBounds(50,275,250,25);
         hasBreakfast.setBounds(250,275,250,25);
+        sleepingFacilityLabel.setBounds(50,350,250,25);
+        hasLinen.setBounds(300,350,175,25);
+        hasTowel.setBounds(500,350,175,25);
 
+    }
+    public void addToContainer(){
         contentPane.add(propertyNameLabel);
         contentPane.add(propertyName);
         contentPane.add(propertyDescLabel);
         contentPane.add(propertyDesc);
         contentPane.add(propertyLocationLabel);
         contentPane.add(propertyLocation);
-
         contentPane.add(hasBreakfastLabel);
         contentPane.add(hasBreakfast);
+        contentPane.add(sleepingFacilityLabel);
+        contentPane.add(hasLinen);
+        contentPane.add(hasTowel);
+    }
 
+    public  HostProperty(){
+
+        super("Host");
+        contentPane = new JPanel();
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        hasLinen = new JCheckBox("Does it have linen?");
+        hasTowel = new JCheckBox("Does it have towel?");
+
+
+
+        setLocationAndSize();
+        addToContainer();
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-//    public static void main(String[] args) {
-//        new HostProperty();
-//    }
+    public static void main(String[] args) {
+        new HostProperty();
+    }
 }
