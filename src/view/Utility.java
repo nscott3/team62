97ae88bb35733f66b838f47bf75b587586c69348
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:23:47 +0000

    change attributes

--- src/view/Utility.java
+++ src/view/Utility.java
@@ -6,14 +6,14 @@ public class Utility {
 	public static void putUtilityInfo(Connection conn, UtilityInfo info) {
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = conn.prepareStatement("INSERT INTO Utility VALUES (?,?,?,?,?,?,?)");
-			pstmt.setByte(0, info.hasCentralHeating);
-			pstmt.setByte(1, info.hasWashingMachine);
-			pstmt.setByte(2, info.hasDryingMachine);
-			pstmt.setByte(3, info.hasFireExtinguisher);
-			pstmt.setByte(4, info.hasSmokeAlarm);
-			pstmt.setByte(5, info.hasFirstAidKit);
-			pstmt.setInt(6, info.facilityNo);
+			pstmt = conn.prepareStatement("INSERT INTO Utility VALUES (default,?,?,?,?,?,?,?)");
+			pstmt.setByte(1, info.hasCentralHeating);
+			pstmt.setByte(2, info.hasWashingMachine);
+			pstmt.setByte(3, info.hasDryingMachine);
+			pstmt.setByte(4, info.hasFireExtinguisher);
+			pstmt.setByte(5, info.hasSmokeAlarm);
+			pstmt.setByte(6, info.hasFirstAidKit);
+			pstmt.setInt(7, info.propertyID);
 			pstmt.executeUpdate();
 			pstmt.close();
 		} catch (SQLException ex) {
@@ -21,23 +21,23 @@ public class Utility {
 		}
 	}
 	
-	public static UtilityInfo getUtilityInfo(Connection conn, int facilityNo) {
+	public static UtilityInfo getUtilityInfo(Connection conn, int propertyID) {
 		UtilityInfo info = new UtilityInfo();
-		info.facilityNo = facilityNo;
+		info.propertyID = propertyID;
 		
 		try {
-			String sql = "SELECT * FROM Utility WHERE facilityNo =" + info.facilityNo;
+			String sql = "SELECT * FROM Utility WHERE propertyID =" + info.propertyID;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			while(rs.next()) {
+				info.utilityID = rs.getInt("utilityID");
 				info.hasCentralHeating = rs.getByte("hasCentralHeating");
 				info.hasWashingMachine = rs.getByte("hasWashingMachine");
 				info.hasDryingMachine = rs.getByte("hasDryingMachine");
 				info.hasFireExtinguisher = rs.getByte("hasFireExtinguisher");
 				info.hasSmokeAlarm = rs.getByte("hasSmokeAlarm");
 				info.hasFirstAidKit = rs.getByte("hasFirstAidKit");
-				info.facilityNo = rs.getInt("facilityNo");
 			}
 			
 			rs.close();
