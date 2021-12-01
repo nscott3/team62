22c867d85f9667c037f2dd86921ae42e92d3cb08
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:10:13 +0000

    change attributes

--- src/view/LivingInfo.java
+++ src/view/LivingInfo.java
@@ -1,11 +1,12 @@
 package view;
 
 public class LivingInfo {
+	public int livingID;
 	public byte hasWifi;
 	public byte hasTV;
 	public byte hasSatellite;
 	public byte hasStreaming;
 	public byte hasDvdPlayer;
 	public byte hasBoardGames;
-	public int facilityNo;
+	public int propertyID;
 }
