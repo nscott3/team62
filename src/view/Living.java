ef30f5ae0c4de9fc04f89e3e6b6f8849a9f62298
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:09:51 +0000

    change attributes

--- src/view/Living.java
+++ src/view/Living.java
@@ -6,14 +6,14 @@ public class Living {
 	public static void putLivingInfo(Connection conn, LivingInfo info) {
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = conn.prepareStatement("INSERT INTO Living VALUES (?,?,?,?,?,?,?)");
-			pstmt.setByte(0, info.hasWifi);
-			pstmt.setByte(1, info.hasTV);
-			pstmt.setByte(2, info.hasSatellite);
-			pstmt.setByte(3, info.hasStreaming);
-			pstmt.setByte(4, info.hasDvdPlayer);
-			pstmt.setByte(5, info.hasBoardGames);
-			pstmt.setInt(6, info.facilityNo);
+			pstmt = conn.prepareStatement("INSERT INTO Living VALUES (default,?,?,?,?,?,?,?)");
+			pstmt.setByte(1, info.hasWifi);
+			pstmt.setByte(2, info.hasTV);
+			pstmt.setByte(3, info.hasSatellite);
+			pstmt.setByte(4, info.hasStreaming);
+			pstmt.setByte(5, info.hasDvdPlayer);
+			pstmt.setByte(6, info.hasBoardGames);
+			pstmt.setInt(7, info.propertyID);
 			pstmt.executeUpdate();
 			pstmt.close();
 		} catch (SQLException ex) {
@@ -21,23 +21,23 @@ public class Living {
 		}
 	}
 	
-	public static LivingInfo getLivingInfo(Connection conn, int facilityNo) {
+	public static LivingInfo getLivingInfo(Connection conn, int propertyID) {
 		LivingInfo info = new LivingInfo();
-		info.facilityNo = facilityNo;
+		info.propertyID = propertyID;
 		
 		try {
-			String sql = "SELECT * FROM Living WHERE facilityNo =" + info.facilityNo;
+			String sql = "SELECT * FROM Living WHERE propertyID =" + info.propertyID;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			while(rs.next()) {
+				info.livingID = rs.getInt("livingID");
 				info.hasWifi = rs.getByte("hasWifi");
 				info.hasTV = rs.getByte("hasTV");
 				info.hasSatellite = rs.getByte("hasSatellite");
 				info.hasStreaming = rs.getByte("hasStreaming");
 				info.hasDvdPlayer = rs.getByte("hasDvdPlayer");
 				info.hasBoardGames = rs.getByte("hasBoardGames");
-				info.facilityNo = rs.getInt("facilityNo");
 			}
 			
 			rs.close();
