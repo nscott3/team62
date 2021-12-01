00df7d0e79ffab09591716fd75369e6f45d2e01b
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:24:09 +0000

    change attributes

--- src/view/UtilityInfo.java
+++ src/view/UtilityInfo.java
@@ -1,11 +1,12 @@
 package view;
 
 public class UtilityInfo {
+	public int utilityID;
 	public byte hasCentralHeating;
 	public byte hasWashingMachine;
 	public byte hasDryingMachine;
 	public byte hasFireExtinguisher;
 	public byte hasSmokeAlarm;
 	public byte hasFirstAidKit;
-	public int facilityNo;
+	public int propertyID;
 }
