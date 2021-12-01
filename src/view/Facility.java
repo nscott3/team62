10b2173a82fffad6b6515d6b73b2c6c432d7e31b
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:00:08 +0000

    Update Facility.java

--- src/view/Facility.java
+++ src/view/Facility.java
@@ -11,38 +11,6 @@ import view.Sleeping;
 import view.Utility;
 
 public class Facility {
-	public static void insertFacilityNo(Connection conn, int hostID) {
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = conn.prepareStatement("INSERT INTO Facility Values (default,?)");
-			pstmt.setInt(1, hostID);
-			pstmt.executeUpdate();
-			pstmt.close();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	public static int lookupFacilityNo(Connection conn, int hostID) {
-		int facilityNo = 0;
-		
-		try {
-			String sql = "SELECT * FROM Facility WHERE hostID =" + hostID;
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
-				facilityNo = rs.getInt("facilityNo");
-			}	
-			
-			rs.close();
-			stmt.close();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		
-		return facilityNo;
-	}
 	
 	public static void registerFacilityInfo(
 			Connection conn, 
