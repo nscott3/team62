1d19495f4888ecef79f86dffb3ea5075b470e431
haeinju <94752412+haeinju@users.noreply.github.com>
2021-12-01 11:13:46 +0000

    change attributes

--- src/view/Outdoor.java
+++ src/view/Outdoor.java
@@ -6,13 +6,13 @@ public class Outdoor {
 	public static void putOutdoorInfo(Connection conn, OutdoorInfo info) {
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = conn.prepareStatement("INSERT INTO Outdoor VALUES (?,?,?,?,?,?)");
-			pstmt.setByte(0, info.hasFreeOnSiteParking);
-			pstmt.setByte(1, info.hasOnRoadParking);
-			pstmt.setByte(2, info.hasPaidParking);
-			pstmt.setByte(3, info.hasPatio);
-			pstmt.setByte(4, info.hasBarbeque);
-			pstmt.setInt(5, info.facilityNo);
+			pstmt = conn.prepareStatement("INSERT INTO Outdoor VALUES (default,?,?,?,?,?,?)");
+			pstmt.setByte(1, info.hasFreeOnSiteParking);
+			pstmt.setByte(2, info.hasOnRoadParking);
+			pstmt.setByte(3, info.hasPaidParking);
+			pstmt.setByte(4, info.hasPatio);
+			pstmt.setByte(5, info.hasBarbeque);
+			pstmt.setInt(6, info.propertyID);
 			pstmt.executeUpdate();
 			pstmt.close();
 		} catch (SQLException ex) {
@@ -20,22 +20,22 @@ public class Outdoor {
 		}
 	}
 	
-	public static OutdoorInfo getOutdoorInfo(Connection conn, int facilityNo) {
+	public static OutdoorInfo getOutdoorInfo(Connection conn, int propertyID) {
 		OutdoorInfo info = new OutdoorInfo();
-		info.facilityNo = facilityNo;
+		info.propertyID = propertyID;
 		
 		try {
-			String sql = "SELECT * FROM Outdoor WHERE facilityNo =" + info.facilityNo;
+			String sql = "SELECT * FROM Outdoor WHERE propertyID =" + info.propertyID;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			while(rs.next()) {
+				info.outdoorID = rs.getInt("outdoorID");
 				info.hasFreeOnSiteParking = rs.getByte("hasFreeOnSiteParking");
 				info.hasOnRoadParking = rs.getByte("hasOnRoadParking");
 				info.hasPaidParking = rs.getByte("hasPaidParking");
 				info.hasPatio = rs.getByte("hasPatio");
 				info.hasBarbeque = rs.getByte("hasBarbeque");
-				info.facilityNo = rs.getInt("facilityNo");
 			}
 			
 			rs.close();
