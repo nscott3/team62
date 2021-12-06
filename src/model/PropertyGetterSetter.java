eaa00b94e4625a65d8a397c3251598b9f4b36fd1
nscott3 <94752703+nscott3@users.noreply.github.com>
2021-12-06 14:01:37 +0000

    Update PropertyGetterSetter.java

--- src/model/PropertyGetterSetter.java
+++ src/model/PropertyGetterSetter.java
@@ -12,42 +12,42 @@ public class PropertyGetterSetter {
     private String endDate;
     private String availability;
     private InfoButton infoBtn;
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	/**
-	 * @return the description
-	 */
-	public String getDescription() {
-		return description;
-	}
-	/**
-	 * @param description the description to set
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	/**
-	 * @return the location
-	 */
-	public String getLocation() {
-		return location;
-	}
-	/**
-	 * @param location the location to set
-	 */
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
     /**
      * @return provides breakfast
      */
@@ -60,18 +60,18 @@ public class PropertyGetterSetter {
     public void setBreakfast(boolean breakfast) {
         this.breakfast = breakfast;
     }
-	/**
-	 * @return the overallScore
-	 */
-	public String getOverallScore() {
-		return OverallScore;
-	}
-	/**
-	 * @param overallScore the overallScore to set
-	 */
-	public void setOverallScore(String overallScore) {
-		OverallScore = overallScore;
-	}
+    /**
+     * @return the overallScore
+     */
+    public String getOverallScore() {
+        return OverallScore;
+    }
+    /**
+     * @param overallScore the overallScore to set
+     */
+    public void setOverallScore(String overallScore) {
+        OverallScore = overallScore;
+    }
     /**
      * @return the availability
      */
@@ -109,15 +109,15 @@ public class PropertyGetterSetter {
         this.endDate = endDate;
     }
     /**
-	 * @return the infoBtn
-	 */
-	public InfoButton getInfoBtn() {
-		return infoBtn;
-	}
-	/**
-	 * @param infoBtn the infoBtn to set
-	 */
-	public void setInfoBtn(InfoButton infoBtn) {
-		this.infoBtn = infoBtn;
-	}
+     * @return the infoBtn
+     */
+    public InfoButton getInfoBtn() {
+        return infoBtn;
+    }
+    /**
+     * @param infoBtn the infoBtn to set
+     */
+    public void setInfoBtn(InfoButton infoBtn) {
+        this.infoBtn = infoBtn;
+    }
 }
